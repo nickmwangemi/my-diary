b4749f478b8ec9bf836bcdea7aceb88ee335a441
nickmwangemi <nickmwangemi@gmail.com>
2021-09-19 10:23:40 +0300

    Improve UX by adding messages for user actions

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     DetailView,
@@ -19,15 +21,17 @@ class EntryDetailView(DetailView):
     model = Entry
 
 
-class EntryCreateView(CreateView):
+class EntryCreateView(SuccessMessageMixin, CreateView):
     model = Entry
     fields = ["title", "content"]
     succeed_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
 
 
-class EntryUpdateView(UpdateView):
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
     model = Entry
     fields = ["title", "content"]
+    success_message = "Your entry was updated!"
 
     def get_success_url(self):
         return reverse_lazy("entry-detail", kwargs={"pk": self.entry.id})
@@ -36,3 +40,8 @@ class EntryUpdateView(UpdateView):
 class EntryDeleteView(DeleteView):
     model = Entry
     success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
